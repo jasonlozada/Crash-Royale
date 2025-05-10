1b417d9330b1b7e01f28634eb2d81d6c50f6d8e2
Jason Lozada <jasonlozada16@g.ucla.edu>
2025-05-09 17:29:56 -0700

    added arena.js and car

---
+++ arena.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+// Create scene
+const scene = new THREE.Scene();
+
+// Load ground texture
+const textureLoader = new THREE.TextureLoader();
+const groundTexture = textureLoader.load('https://threejs.org/examples/textures/checker.png');
+groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set(100, 100);
+
+const groundMaterial = new THREE.MeshPhongMaterial({ map: groundTexture, side: THREE.DoubleSide });
+const groundGeometry = new THREE.PlaneGeometry(500, 500);
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+scene.add(ground);
+
+// Lighting
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(10, 20, 10);
+scene.add(light);
+const ambient = new THREE.AmbientLight(0x404040);
+scene.add(ambient);
+
+// Export scene and groundTexture globally so main.js can access
+window.scene = scene;
+window.groundTexture = groundTexture;
